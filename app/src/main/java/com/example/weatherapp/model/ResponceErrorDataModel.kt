0325f2466fe1494564fa5acb41069e1e50ab0c16
leva <leva@live.ru>
2023-02-24 21:20:20 +0100

    Немного пригладил код

--- app/src/main/java/com/example/weatherapp/model/ResponceErrorDataModel.kt
+++ app/src/main/java/com/example/weatherapp/model/ResponceErrorDataModel.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapp.model
 
 data class ResponceErrorDataModel(
-    val code: Int,
-    val message: String
+    val code: Int = 0,
+    val message: String = "null",
+    val status:String = "null"
 ):java.io.Serializable
--- app/src/main/java/com/example/weatherapp/viewmodel/network/requestToApi.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/network/requestToApi.kt
@@ -24,16 +24,14 @@ class RequestToApi(val url: String, val city: String, val key: String ) {
             override fun onResponse(call: Call<WeatherParse>, response: Response<WeatherParse>) {
                 when(response.code()){
                     // обработка положительного результата от сервера
-                    200 -> if (response.isSuccessful) {
-                        Log.d("TestMsg","Request 200: ${response.body()}")
-                        responseWeatherData.postValue(response.body())
-                    }
+                    200 -> responseWeatherData.postValue(response.body())
                     // обработка ошибки от сервера
                     else -> {Log.d("TestMsg","Err: ${response.errorBody()}, responce.body:${response.body()}")} /*400,401,403  | JSONObject(response.errorBody()!!.string()).getJSONObject("error").getString("message")*/
                 }
             }
             override fun onFailure(call: Call<WeatherParse>, t: Throwable) {
                 Log.d("TestMsg","onFailure Err: $t")// обработка ошибки соединения
+                return
             }
         })
     }
