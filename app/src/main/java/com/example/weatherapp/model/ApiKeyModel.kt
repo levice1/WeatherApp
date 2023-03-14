faea98b5afc2b08748f3de3f95338e33fa502c45
levice1 <124053072+levice1@users.noreply.github.com>
2023-03-14 19:39:12 +0100

    Update ApiKeyModel.kt

--- app/src/main/java/com/example/weatherapp/model/ApiKeyModel.kt
+++ app/src/main/java/com/example/weatherapp/model/ApiKeyModel.kt
@@ -1,6 +1,6 @@
 package com.example.weatherapp.model
 
 class ApiKeyModel {
-    fun getApi():String{ return "8475455780504d6c9a9224201230903" }
+    fun getApi():String{ return "KEY" }
     fun getUrl():String{ return "http://api.weatherapi.com/" }
 }
